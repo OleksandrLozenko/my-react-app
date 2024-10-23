a4b6716c04752c49b75da8fe2f6e8a6f0ed3551b
Oleksandr Lozenko <dendroscript@gmail.com>
2024-10-23 16:30:45 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
-
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div>
-      {
-
-      }
-      <Header/>
-    </div>
+    <Router basename="/my-react-app">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
